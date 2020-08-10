70cc53a0ba4139f8ff5ed72e9b3f8ecda88a5af3
unknown <harisson.nas@gmail.com>
2020-08-10 16:45:25 -0300

    SpringBoot and REST Repositories

---
+++ 15 RestWithSpringBootUdemy Pagination/src/main/java/br/com/erudio/config/WebConfig.java
@@ -0,0 +1,64 @@
+package br.com.erudio.config;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import br.com.erudio.serialization.converter.YamlJackson2HttpMessageConverter;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+	private static final MediaType MEDIA_TYPE_YML = MediaType.valueOf("application/x-yaml");
+
+	public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+		converters.add(new YamlJackson2HttpMessageConverter());
+	}
+	
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+			.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");	
+	}
+
+	@Override
+	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+		
+		/* By Header. */
+		configurer.favorParameter(false)
+			.ignoreAcceptHeader(false)
+			.useRegisteredExtensionsOnly(false)
+			.defaultContentType(MediaType.APPLICATION_JSON)
+			.mediaType("json",MediaType.APPLICATION_JSON)
+			.mediaType("xml",MediaType.APPLICATION_XML)
+			.mediaType("x-yaml", MEDIA_TYPE_YML);
+		
+		/*
+		 * By Extension. Example=> localhost:8080/person.x-yaml
+		 * configurer.favorParameter(false) .ignoreAcceptHeader(false)
+		 * .defaultContentType(MediaType.APPLICATION_JSON)
+		 * .mediaType("json",MediaType.APPLICATION_JSON) .mediaType("xml",
+		 * MediaType.APPLICATION_XML);
+		 */
+
+		/*
+		 * By Query Param. Example=> localhost:8080/person?mediaType=xml
+		 * configurer.favorParameter(true) .parameterName("mediaType")
+		 * .ignoreAcceptHeader(true) .useRegisteredExtensionsOnly(false)
+		 * .defaultContentType(MediaType.APPLICATION_JSON) .mediaType("json",
+		 * MediaType.APPLICATION_JSON) .mediaType("xml", MediaType.APPLICATION_XML);
+		 */
+
+		/*
+		 * YAML Support Example configurer.favorParameter(false)
+		 * .ignoreAcceptHeader(false) .useRegisteredExtensionsOnly(false)
+		 * .defaultContentType(MediaType.APPLICATION_JSON)
+		 * .mediaType("json",MediaType.APPLICATION_JSON) .mediaType("xml",
+		 * MediaType.APPLICATION_XML) .mediaType("x-yaml", MEDIA_TYPE_YML);
+		 */
+	}
+}
