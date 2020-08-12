9ba5b77b7677885a21986d3dd98a8bac31620d18
unknown <harisson.nas@gmail.com>
2020-08-12 18:29:21 -0300

    Deploy in AWS

---
+++ 18 RestWithSpringBootUdemy Deploy in AWS/RestWithSpringBootUdemy/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,8 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'Goiânia-Goias-Brasil', 'Yasmin', 'Female', 'Nascimento'),
+	(2, 'Goiânia-Goias-Brasil', 'Harisson', 'Male', 'Nascimento'),
+	(3, 'Goiânia-Goias-Brasil', 'Estopinha', 'Male', 'Nascimento'),
+	(5, 'Goiânia-Goias-Brasil', 'Rayenny', 'Female', 'Nascimento'),
+	(6, 'Goiânia-Goias-Brasil', 'Monica', 'Female', 'Nascimento'),
+	(8, 'Goiânia-Goias-Brasil', 'Teste', 'Male', 'Sercarovisk'),
+	(9, 'Goiânia-Goias-Brasil', 'Teste v1', 'Male', 'Sercarovisk');
